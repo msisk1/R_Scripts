d6734d9ab537a598bdbea09ee1fe343c631f263e
mat <msisk1@nd.edu>
2016-11-29 09:50:23 -0500

    Made it plot if files exist

--- Hui_LCP_China.R
+++ Hui_LCP_China.R
@@ -9,74 +9,96 @@ lapply(packages, require, character.only=T) # load the packages, if they don't l
 
 
 
-setwd("E:/GISWork_2/Hui_China") #WIndows maching
-# setwd("/home/matthew/Documents/HUI") #Linux machine
+# setwd("E:/GISWork_2/Hui_China") #WIndows maching
+setwd("/home/matthew/Documents/HUI") #Linux machine
+
+
+#Functions
+scaleBB <- function(box){
+  # box <- bbox(site.points[1:2,])
+  # box <- bbox(site.points[x:x+1,])
+  ew <- (box["E","max"] -box["E","min"]) / 4
+  ns <- (box["N","max"] -box["N","min"]) / 4
+  if (ns < 1000){
+    ns <- ew
+  }
+  M <- matrix(c(-ew,-ns,ew,ns), ncol = 2)
+  return(box + M)
+}
+altDiff <- function(x){x[2] - x[1]}
+calculateConductance <- function(in.raster){
+  hd <- transition(in.raster, altDiff, 8, symm=FALSE)
+  slope <- geoCorrection(hd)
+  # plot(dem.low)
+  adj <- adjacent(in.raster, cells=1:ncell(in.raster), pairs=TRUE, directions=8)
+  speed <- slope
+  speed[adj] <- 6 * exp(-3.5 * abs(slope[adj] + 0.05))
+  cond <- geoCorrection(speed)  
+  return(cond)
+}
+
+
+
+#Variables
 latlong <- "+init=epsg:4326" #This is the espg code for the WGS 1984 geographic projection
 
-
+#open the raster 
 dem <- raster("chgis_dem.tif")   #load the raster: Make sure the auxillary files are present
+
+dem[dem == -32768] <- NA #Filtering out NA Values
+
 dem.low <-aggregate(dem, fact =  5)
 proj4string(dem) #verify that this is non-NA
 
 #Open the list of points
-
-
-trial.sites <- read.csv("test_locations.csv")  #Read the table with Lat and lon
-# trial.sites.table <- trial.sites
-coordinates(trial.sites) <- ~E + N             #Define the coordinates to convert it to a spatial points data frame
-proj4string(trial.sites) <- CRS(latlong)  
-
-trial.sites <- spTransform(trial.sites, CRS(proj4string(dem)))
-(proj4string(trial.sites) == proj4string(dem))
-
-
-scaleBB <- function(box){
-        # box <- bbox(trial.sites[1:2,])
-        # box <- bbox(trial.sites[x:x+1,])
-        ew <- (box["E","max"] -box["E","min"]) / 4
-        ns <- (box["N","max"] -box["N","min"]) / 4
-        M <- matrix(c(-ew,-ns,ew,ns), ncol = 2)
-        return(box + M)
-}
+site.points.all <- read.csv("All_Locations_2016-10-11.csv")  #Read the table with Lat and lon
+# site.points.table <- site.points
+coordinates(site.points.all) <- ~E + N             #Define the coordinates to convert it to a spatial points data frame
+proj4string(site.points.all) <- CRS(latlong)  
+site.points.all <- spTransform(site.points.all, CRS(proj4string(dem)))
+(proj4string(site.points.all) == proj4string(dem))
 
 
 
-
-altDiff <- function(x){x[2] - x[1]}
-
-calculateConductance <- function(in.raster){
-        hd <- transition(in.raster, altDiff, 8, symm=FALSE)
-        slope <- geoCorrection(hd)
-        # plot(dem.low)
-        adj <- adjacent(in.raster, cells=1:ncell(in.raster), pairs=TRUE, directions=8)
-        speed <- slope
-        speed[adj] <- 6 * exp(-3.5 * abs(slope[adj] + 0.05))
-        cond <- geoCorrection(speed)  
-        return(cond)
-}
-
-
-first <- T
-for (x in 1:(nrow(trial.sites)-1)){
-        # x <- 1
-        print(paste(x,x+1))
-        clip_dem <- crop(dem,scaleBB(bbox(trial.sites[x:(x+1),])))
-        Conductance <- calculateConductance(clip_dem)
-        each.line <- shortestPath(Conductance, trial.sites@coords[x,], trial.sites@coords[x+1,], output="SpatialLines")
-        if (first){
-                all <- each.line
-                first <- FALSE
-        }else{
-                all <- all + each.line
-        }
-}
-
+all.path.descriptions <- unique(site.points.all$Desc)
 plot(dem.low)
-plot(trial.sites, add=T)
-plot(all, add=TRUE)
-all.df <- SpatialLinesDataFrame(all, data.frame(ID = c(1:length(all))))
 
-writeOGR(obj = all.df, dsn= ".", layer = "test", driver="ESRI Shapefile") #writes the spatial points data frame as a shapefile
 
-# plot(AtoB, add=TRUE)
-# plot(trial.sites,add=T)
+for (each.path in all.path.descriptions){
+  print(each.path)
+  out.name <- paste("Route_",as.numeric(all.path.descriptions[all.path.descriptions==each.path]),sep="")
+  site.points <- site.points.all[(site.points.all$Desc == each.path),]
+  if (file.exists(paste(out.name,".shp",sep=""))){
+    print("ouput already exists")
+    all <- readOGR(dsn=".", layer = out.name)
+    #next()
+  }else{
+    # print(site.points)
+    first <- T
+    for (x in 1:(nrow(site.points)-1)){
+      print(paste(x,x+1))
+      clip_dem <- crop(dem,scaleBB(bbox(site.points[x:(x+1),])))
+      Conductance <- calculateConductance(clip_dem)
+      each.line <- shortestPath(Conductance, site.points@coords[x,], site.points@coords[x+1,], output="SpatialLines")
+      if (first){
+        all <- each.line
+        first <- FALSE
+      }else{
+        all <- all + each.line
+      }#end else
+    }# end interior for loop
+    
+    all.df <- SpatialLinesDataFrame(all, data.frame(ID = c(1:length(all))))
+    writeOGR(obj = all.df, dsn= ".", layer = out.name, driver="ESRI Shapefile",overwrite_layer=T) #writes the spatial points data frame as a shapefile
+    
+  }#end else if file exists
+  plot(site.points, add=T)
+  plot(all, add=TRUE)
+  
+}#end exterior for loop
+
+
+
+#Adding a china boundry
+a<-getData("GADM",country="China",level=0)
+plot(spTransform(a, CRS(proj4string(dem))), col = "transparent", add=T)
