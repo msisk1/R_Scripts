ddd29ee80a5d6377041ab9b0cd2e2ae95fee0e70
Matthew Sisk <MSisk1@nd.edu>
2017-07-11 09:55:01 -0400

    Lead near final

--- Lead_start.R
+++ Lead_start.R
@@ -3,7 +3,7 @@ rm(list=ls(all=TRUE)) # clear memory
 
 
 
-packages<- c("rgdal","sp","lubridate","ggplot2","doBy","dplyr") # list the packages that you'll need
+packages<- c("rgdal","sp","lubridate","ggplot2","doBy","dplyr","foreign","grid") # list the packages that you'll need
 
 lapply(packages, require, character.only=T) # load the packages, if they don't load you might need to install them first
 
@@ -14,6 +14,10 @@ lapply(packages, require, character.only=T) # load the packages, if they don't l
 
 # setwd("E:/GISWork_2/Hui_China") #WIndows maching
 setwd("E:/GISWork_2/Beidinger_Lead") #Linux machine
+
+
+spss2date <- function(x) as.Date(x/86400, origin = "1582-10-14")
+
 
 which <- "Tracts"
 # which <- "Block Groups"
@@ -43,18 +47,24 @@ if (which == "Tracts"){
 
 
 
-# lead.data <- read.csv("HBB312017.csv", stringsAsFactors = F)                  #OLD DATA
-lead.data <- read.csv("UniqueID_BG_May2017.csv", stringsAsFactors = F)
+# lead.data.old <- read.csv("HBB312017.csv", stringsAsFactors = F)                  #OLD DATA
+lead.data <- read.csv("N:\\no_tests_final_unique_IDs_removed_UPDATED_Sisk.csv", stringsAsFactors = F)
+
+# lead.data<- read.csv("UniqueID_BG_May2017.csv", stringsAsFactors = F)
+
+#Attempting to read Michelle's spss file
+# lead.data <- read.spss("no_tests_final_unique_IDs_removed_UPDATED_Sisk.sav", to.data.frame=TRUE)
+# write.csv(dataset,"no_tests_final_unique_IDs_removed_UPDATED.csv",row.names = F)
 
 
-lead.data$Month <- month(as.Date(substr(lead.data$SPEC_DT,1,10), format = "%m/%d/%Y"))
-lead.data$year <- year(as.Date(substr(lead.data$SPEC_DT,1,10), format = "%m/%d/%Y"))
-lead.data$real_spec <- as.Date(substr(lead.data$SPEC_DT,1,10), format = "%m/%d/%Y")
-
-lead.data$age <- (as.Date(substr(lead.data$SPEC_DT,1,10), format = "%m/%d/%Y")- as.Date(substr(lead.data$DOB,1,10), format = "%m/%d/%Y")) / 365
+lead.data$Month <- month(as.Date(lead.data$SPEC_DT, format = "%m/%d/%Y"))
+lead.data$year <- year(as.Date(lead.data$SPEC_DT, format = "%m/%d/%Y"))
+lead.data$real_spec <- as.Date(lead.data$SPEC_DT, format = "%m/%d/%Y")
+lead.data$real_dob <- as.Date(lead.data$DOB, format = "%m/%d/%Y")
+lead.data$age <- (lead.data$real_spec- lead.data$real_dob)/365
 #Tossing values we don't need
-# lead.data <- lead.data[which (lead.data$code != "Postal" & lead.data$code != "Locality"),]
-# lead.data <- lead.data[which (lead.data$age >=0 & lead.data$age < 7),]
+ lead.data <- lead.data[which (lead.data$code != "Postal" & lead.data$code != "Locality"),]
+ lead.data <- lead.data[which (as.integer(lead.data$AGE_TRUNC) >=0 & as.integer(lead.data$AGE_TRUNC) < 7),]
 
 #keeping only the first record (i.e. converting from tests to individuals)
 if (individuals){
@@ -62,8 +72,26 @@ if (individuals){
                 group_by(UniqueID) %>%
                 arrange(real_spec) %>%
                 slice(1L)
-        lead.data <-data.frame(lead.ind)
+        lead.ind <-data.frame(lead.ind)
+        # 
 }
+
+#making table of tests vs individual
+yearly.table.of.tests.individuals <- rbind( table(lead.data$year),table(lead.ind$year))
+barplot(yearly.table.of.tests.individuals, beside = T,col = c("blue","green"),legend = c("Tests","Individuals"),main="Children 7 and under", xlab = "Year",args.legend = list(x ='topright', bty='n'))
+lead.data <- lead.ind
+
+
+#Another Table for number of elevated individuals
+
+table(lead.ind$year)
+as.data.frame(table(lead.ind[which(lead.ind$PB_CDC>=5 &lead.ind$PB_CDC < 10),]$year))
+
+as.data.frame(table(lead.ind[which(lead.ind$pb_grp == 3),]$year))
+
+table(lead.ind$year,lead.ind$pb_grp)
+table(lead.ind[which(lead.ind$PB_CDC >= 10),]$year)
+
 
 preg <- lead.data[which (lead.data$PREGNANT =="Y"),]
 
@@ -133,7 +161,13 @@ pdf(out.pdf)
 spplot(st.joes.lead[which (st.joes.lead$City == 2 ),], c("p_2007","p_2008","p_2005","p_2006"),names.attr = c("2007","2008","2005","2006"))
 spplot(st.joes.lead[which (st.joes.lead$City == 2 ),], c("p_2011","p_2012","p_2009","p_2010"),names.attr = c("2011","2012","2009","2010"))
 spplot(st.joes.lead[which (st.joes.lead$City == 2 ),], c("p_2015","p_all","p_2013","p_2014"),names.attr = c("2015","All Years","2013","2014"))
-spplot(st.joes.lead[which (st.joes.lead$City == 2 ),], c("p_all"),main = c("All Years"))
+
+
+spplot(st.joes.lead[which (st.joes.lead$City == 2 ),], c("p_all"),main = c("2005-2015"))
+grid.text("Percentage of children (7 and under) with lead test > 5", x=unit(0.95, "npc"), y=unit(0.50, "npc"), rot=-90)
+
+spplot(st.joes.lead[which (st.joes.lead$City == 2 ),], c("p_2015"),main = c("2015"))
+grid.text("Percentage of children (5 and under) with lead test > 5", x=unit(0.95, "npc"), y=unit(0.50, "npc"), rot=-90)
 
 
 # lead.agg2 <- merge(lead.agg2,lead.agg,by="GEO_ID")
@@ -182,5 +216,38 @@ dev.off()
 #DO NUMBER OF UNIQUE KIDS BASED ON CENSUS TRACT POPULATION
 
 #This section is for creating a table
+lead.data$AGEL <- as.double(lead.data$AGE)
+lead.data$AGE_TRUNC <- as.integer(lead.data$AGE_TRUNC)
+lead.data$ageclass3 <-  ifelse(lead.data$AGE_TRUNC == 0, "0-1", 
+                        ifelse(lead.data$AGE_TRUNC == 1, "1-1.99",
+                        ifelse(lead.data$AGE_TRUNC == 2,"2-2.99",
+                        ifelse(lead.data$AGE_TRUNC == 3,"3-3.99",
+                        ifelse(lead.data$AGE_TRUNC == 4,"4-4.99",
+                        ifelse(lead.data$AGE_TRUNC == 5,"5-5.99",
+                        ifelse(lead.data$AGE_TRUNC == 6,"6- 6.99",
+                        ifelse(lead.data$AGE_TRUNC <=18,"7-18",
+                        ifelse(lead.data$AGE_TRUNC <=45,"18-45",
+                        ifelse(lead.data$AGE_TRUNC >45,"45 +","ERROR"))))))))))
+
+# aggregate(PB_RESULT~ageclass3, data=lead.data[which(lead.data$PB_CDC > 0),], FUN=mean)
+# summaryBy(PB_RESULT ~ ageclass3, data = lead.data,FUN=c(length, mean,sd))
+
+demog.table03 <- merge(summaryBy(PB_CDC ~ ageclass3, data = lead.data,FUN=c(length, mean,sd,max),var.names = "all",fun.names=c("n","mean","sd","max")),summaryBy(PB_CDC ~ ageclass3, data = lead.data[which(lead.data$PB_CDC > 0),],FUN=c(length, mean,sd),var.names = "ov0",fun.names=c("n","mean","sd")),by="ageclass3")
+# write.csv(demog.table03,"DemographicTable_under7.csv",row.names = F)
 
 
+
+
+#Section for creating census tracts table
+pov.table <- read.csv("OtherData//ACS_Tables//ACS_2015_5year_Poverty.csv",stringsAsFactors = F)
+census2010 <-read.csv("OtherData//ACS_Tables//C2010_Children.csv",stringsAsFactors = F)
+acsAll <-read.csv("OtherData//ACS_Tables//ACS_5yr_compiled_NoHeader.csv",stringsAsFactors = F)
+all.census <- merge(census2010,pov.table,by="Geo_FIPS",all=T)
+all.census <- merge(all.census,acsAll,by="Geo_FIPS",all=T)
+all.census$GEO_ID <- paste("1400",substr(all.census$Geo_GEOID.y,3,25),sep="")
+
+all.census.final <- merge(st.joes.lead@data,all.census,by="GEO_ID",all=T)
+all.census.final$hh_povPer <- 100* all.census.final$X2015_povFam / all.census.final$X2015_numFam
+
+exporter <- all.census.final[,c("GEO_ID","NAME","YearBuilt","N_all","N_elev","p_all","e_2015","t_2015","p_2015","hh_povPer","X2015_SE_T007_002")]
+write.csv(exporter,"Report//TractTable.csv",row.names = F)
