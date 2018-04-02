07bd5672a03dd1589219e8fb0d4200943b00e25b
mat <msisk1@nd.edu>
2018-04-02 13:02:46 -0400

    edits to Hui

--- Hui_LCP_China.R
+++ Hui_LCP_China.R
@@ -1,7 +1,10 @@
 # install.packages(("gdistance"))
 rm(list=ls(all=TRUE)) # clear memory
 
-
+#Todo:
+# Get the polylines all in one
+# Make it so changes to the factor for the route names does not change the order
+# 
 
 packages<- c("rgdal","raster","gdistance","rgeos","sp","maptools") # list the packages that you'll need
 
@@ -12,6 +15,10 @@ lapply(packages, require, character.only=T) # load the packages, if they don't l
 # setwd("E:/GISWork_2/Hui_China") #WIndows maching
 setwd("/home/matthew/Documents/HUI") #Linux machine
 
+exten <- "2018-03-27"
+in.file <- paste("All_Locations_", exten, ".csv", sep="")
+in.file <-"1 West Han Routes.csv"
+out.path <- paste("paths_",exten,sep="")
 
 #Functions
 scaleBB <- function(box){
@@ -46,12 +53,18 @@ latlong <- "+init=epsg:4326" #This is the espg code for the WGS 1984 geographic 
 dem <- raster("chgis_dem.tif")   #load the raster: Make sure the auxillary files are present
 
 dem[dem == -32768] <- NA #Filtering out NA Values
+if (!file.exists("chgis_dem_low.tif")){
+  print("no.file")
+  dem.low <-aggregate(dem, fact =  5)
+  writeRaster(dem.low,"chgis_dem_low.tif")
+}else{
+  dem.low <- raster("chgis_dem_low.tif")
+}
 
-dem.low <-aggregate(dem, fact =  5)
 proj4string(dem) #verify that this is non-NA
 
 #Open the list of points
-site.points.all <- read.csv("All_Locations_2018-2-16.csv")  #Read the table with Lat and lon
+site.points.all <- read.csv(in.file)  #Read the table with Lat and lon
 # site.points.table <- site.points
 coordinates(site.points.all) <- ~E + N             #Define the coordinates to convert it to a spatial points data frame
 proj4string(site.points.all) <- CRS(latlong)  
@@ -68,11 +81,11 @@ for (each.path in all.path.descriptions){
   print(each.path)
   xxx<-xxx+1
   out.name <- paste("Route_",as.numeric(all.path.descriptions[all.path.descriptions==each.path]),sep="")
-  out.name2 <- paste("RouteSimp_",as.numeric(all.path.descriptions[all.path.descriptions==each.path]),sep="")
+  # out.name2 <- paste("RouteSimp_",as.numeric(all.path.descriptions[all.path.descriptions==each.path]),sep="")
   site.points <- site.points.all[(site.points.all$Desc == each.path),]
-  if (file.exists(paste(out.name,".shp",sep=""))){
+  if (file.exists(paste(out.path,"/",out.name,".shp",sep=""))){
     print("ouput already exists")
-    all <- readOGR(dsn=".", layer = out.name)
+    all <- readOGR(dsn=out.path, layer = out.name)
     #next()
   }else{
     # print(site.points)
@@ -90,15 +103,13 @@ for (each.path in all.path.descriptions){
       }#end else
     }# end interior for loop
     all <- gLineMerge(all)
-    # row.names(all)<-xxx
-    # all@lines[[1]]@ID <- as.character(xxx)
-    all2 <- gSimplify(all, 500, topologyPreserve=FALSE)
+    # all2 <- gSimplify(all, 500, topologyPreserve=FALSE)
     
     all.df <- SpatialLinesDataFrame(all, data.frame(Name = each.path))
-    all.df2 <- SpatialLinesDataFrame(all2, data.frame(Name = each.path))
+    # all.df2 <- SpatialLinesDataFrame(all2, data.frame(Name = each.path))
     # all.df <- SpatialLinesDataFrame(all, data.frame(ID = c(1:length(all))))
-    writeOGR(obj = all.df, dsn= ".", layer = out.name, driver="ESRI Shapefile",overwrite_layer=T) #writes the spatial points data frame as a shapefile
-    writeOGR(obj = all.df2, dsn= ".", layer = out.name2, driver="ESRI Shapefile",overwrite_layer=T) #writes the spatial points data frame as a shapefile
+    writeOGR(obj = all.df, dsn= out.path, layer = out.name, driver="ESRI Shapefile",overwrite_layer=T) #writes the spatial points data frame as a shapefile
+    # writeOGR(obj = all.df2, dsn= out.path, layer = out.name2, driver="ESRI Shapefile",overwrite_layer=T) #writes the spatial points data frame as a shapefile
     
   }#end else if file exists
   plot(site.points, add=T)
@@ -116,7 +127,7 @@ for (each.path in all.path.descriptions){
 
 #Adding a china boundry
 china.outline<-getData("GADM",country="China",level=0)
-writeOGR(obj=china.outline,dsn=".",layer="china_outline",driver="ESRI Shapefile",overwrite_layer=T)
+# writeOGR(obj=china.outline,dsn=".",layer="china_outline",driver="ESRI Shapefile",overwrite_layer=T)
 plot(spTransform(china.outline, CRS(proj4string(dem))), col = "transparent", add=T)
 
 
